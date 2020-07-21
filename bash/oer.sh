b70a94b731e75e51e27ccde63af97ccfaebf195f
Tim Loden <tim.m.loden@gmail.com>
2020-07-21 10:32:34 -0700

    added argument for wp path

--- bash/oer.sh
+++ bash/oer.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-export $(grep -v '^#' .env | xargs)
-
 #Download OER data file
 echo "Downloading OER feed file..." ;
-wp rp download_oer --path=$WP_PATH ;
+wp rp download_oer --path=$1 ;
 
 #Process OER data file
 echo "Processing OER feed file..." ;
-wp rp process_oer --path=$WP_PATH ;
+wp rp process_oer --path=$1 ;
 
 echo "Process completed" ;
