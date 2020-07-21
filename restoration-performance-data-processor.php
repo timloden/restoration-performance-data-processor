dcb6f97778a21f764d53e856fbdec2034186f50a
Tim Loden <tim.m.loden@gmail.com>
2020-07-21 08:03:13 -0700

    updated version

--- restoration-performance-data-processor.php
+++ restoration-performance-data-processor.php
@@ -7,7 +7,7 @@
  * Author URI:      https://timloden.com
  * Text Domain:     restoration-performance-data-processor
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.2
  *
  * @package         Restoration_Performance_Data_Processor
  */
