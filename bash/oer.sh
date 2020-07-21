03b7bc061e3135ed21b97caf59901c5881fe424a
Tim Loden <tim.m.loden@gmail.com>
2020-07-21 07:53:16 -0700

    added wp path to commads in sh

--- bash/oer.sh
+++ bash/oer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export $(grep -v '^#' .env | xargs)
+
 #Download OER data file
 echo "Downloading OER feed file..." ;
-wp rp download_oer ;
+wp rp download_oer --path=$WP_PATH ;
 
 #Process OER data file
 echo "Processing OER feed file..." ;
-wp rp process_oer ;
+wp rp process_oer --path=$WP_PATH ;
 
 echo "Process completed" ;
