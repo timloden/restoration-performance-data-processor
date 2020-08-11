c4eb781bd4544976cfbcbb91882d5d34e232daa4
Tim Loden <tim.m.loden@gmail.com>
2020-08-11 07:36:05 -0700

    version bump to 1.0.0

--- restoration-performance-data-processor.php
+++ restoration-performance-data-processor.php
@@ -7,7 +7,7 @@
  * Author URI:      https://timloden.com
  * Text Domain:     restoration-performance-data-processor
  * Domain Path:     /languages
- * Version:         0.1.4
+ * Version:         1.0.0
  *
  * @package         Restoration_Performance_Data_Processor
  */
