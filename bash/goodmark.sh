7a2ccf8e4c8540a7ca798f0568cc5a29bc551df6
Tim Loden <tim.m.loden@gmail.com>
2021-01-29 11:21:21 -0800

    added download exsisting files to shell scripts

--- bash/goodmark.sh
+++ bash/goodmark.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Download exsisting Goodmark data file
+echo "Downloading exsisting Goodmark feed file..." ;
+/usr/local/bin/wp rp download_existing_goodmark --path=$1 ;
+
 #Download Goodmark data file
 echo "Downloading Goodmark feed file..." ;
 /usr/local/bin/wp rp download_goodmark --path=$1 ;
--- bash/oer.sh
+++ bash/oer.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Download exsisting OER data file
+echo "Downloading exsisting OER feed file..." ;
+/usr/local/bin/wp rp download_existing_oer --path=$1 ;
+
 #Download OER data file
 echo "Downloading OER feed file..." ;
 /usr/local/bin/wp rp download_oer --path=$1 ;
