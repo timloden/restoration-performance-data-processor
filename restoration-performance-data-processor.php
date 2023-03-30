e5cc2c0a9607b24dca69634356ac935f37e75ae0
Tim Loden <tim.m.loden@gmail.com>
2023-03-29 21:45:24 -0700

    updated oer for sftp

--- restoration-performance-data-processor.php
+++ restoration-performance-data-processor.php
@@ -384,38 +384,29 @@ class RP_CLI {
     }
 
 	public function download_oer() {
-        // TODO move this back to SFTP once OER is uploading file to new server
+
+        $ftp_server = get_option( '_oer_host' );
+        $ftp_user_name = get_option( '_oer_user' );
+        $ftp_user_pass = get_option( '_oer_pass' );
+        $server_file = get_option( '_oer_file_name' );
+
+        $uploads = wp_upload_dir();
+        $dir = $uploads['basedir'] . '/vendors/oer/';
+
+        $local_file = 'oer-temp.csv';
+
+        try {
         
-       // define our files
-       $local_file = 'oer-temp.csv';
-       //$server_file = 'RPC.csv';
-       $server_file = get_option( '_oer_file_name' );
+        $sftp = new SFTP($ftp_server);
+        $sftp->login($ftp_user_name, $ftp_user_pass);
 
-       $uploads = wp_upload_dir();
-       $dir = $uploads['basedir'] . '/vendors/oer/';
+        $sftp->get($server_file, $dir . $local_file);
 
-       $ftp_server = get_option( '_oer_host' );
-       $ftp_user_name = get_option( '_oer_user' );
-       $ftp_user_pass = get_option( '_oer_pass' );
+        WP_CLI::success( 'Successfully saved to ' . $dir . $local_file );
 
-       // set up basic connection
-       $conn_id = ftp_connect($ftp_server);
-
-       // login with username and password
-       $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
-       ftp_pasv($conn_id, true);
-
-       // try to download $server_file and save to $local_file
-       if (ftp_get($conn_id, $dir.$local_file, $server_file, FTP_BINARY)) {
-           // echo "Successfully written to $local_file\n";
-           WP_CLI::line( 'Downloading...' );
-           WP_CLI::success( 'Successfully written to ' . $dir . $local_file );
-       } else {
-           WP_CLI::error( 'There was a problem' );
-       }
-
-       // close the connection
-       ftp_close($conn_id);
+        } catch (Exception $e) {
+            WP_CLI::error( $e->getMessage() );
+        }
 
     }
     
